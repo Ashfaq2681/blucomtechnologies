73a6c7abace377e01bafd597c5352fa1832bda79
Ashfaq2681 <91347989+Ashfaq2681@users.noreply.github.com>
2025-02-26 02:26:43 +0500

    v2.1.5

--- frontend/dist/assets/Dashboard-DCKWfDAF.js
+++ frontend/dist/assets/Dashboard-DCKWfDAF.js
@@ -1,4 +1,4 @@
-import{r as x,j as S,u as er}from"./index-C4MQiQu-.js";/**
+import{r as x,j as S,u as er}from"./index-DfR6H9K3.js";/**
  * @license lucide-react v0.475.0 - ISC
  *
  * This source code is licensed under the ISC license.
