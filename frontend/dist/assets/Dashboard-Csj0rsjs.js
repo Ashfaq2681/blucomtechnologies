160b60e725ab7ae19d8386b00a5e5acd530f3900
Ashfaq2681 <91347989+Ashfaq2681@users.noreply.github.com>
2025-02-25 17:22:58 +0500

    v2.1.5

--- frontend/dist/assets/Dashboard-Csj0rsjs.js
+++ frontend/dist/assets/Dashboard-Csj0rsjs.js
@@ -1,4 +1,4 @@
-import{r as x,j as S,u as er}from"./index-uiXaQjmh.js";/**
+import{r as x,j as S,u as er}from"./index-CibS3FcA.js";/**
  * @license lucide-react v0.475.0 - ISC
  *
  * This source code is licensed under the ISC license.
