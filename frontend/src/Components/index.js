65655ee52850e4adb48c15af91780c7ea69b3568
Ashfaq2681 <91347989+Ashfaq2681@users.noreply.github.com>
2025-02-26 02:00:31 +0500

    v2.1.5

--- frontend/src/Components/index.js
+++ frontend/src/Components/index.js
@@ -1,6 +1,6 @@
 export const nav_links = [
   { title: "About us", link: "/About" },
-  { title: "Work", link: "/portfolio" },
+  { title: "Work", link: "/Portfolio" },
   { title: "Blog", link: "/blogsingle" },
   { title: "Ideas", link: "/ideas" },
   { title: "News", link: "/news" },
